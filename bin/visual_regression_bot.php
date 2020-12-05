aa76d97813583129902bbb708a639c48f06b65de
Alex Moreno <alejandro.moreno@acquia.com>
2020-12-05 18:11:56 +0000

    Added new command to automate regression checks.

--- bin/visual_regression_bot.php
+++ bin/visual_regression_bot.php
@@ -7,6 +7,7 @@ use Symfony\Component\Console\Application;
 
 $application = new Application("Visual Regression Bot");
 $application->add(new \Command\CrawlSitesCommand());
+$application->add(new \Command\CompareCrawlsCommand());
 $application->add(new \Command\Acquia\GenerateSiteListCommand());
 $application->add(new \Command\Acquia\AcsfCrawlSitesCommand());
 
