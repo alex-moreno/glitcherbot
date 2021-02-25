0b2270b947f735aac230674f691a76e400ef2490
Alex Moreno <alejandro.moreno@acquia.com>
2021-02-25 14:48:41 +0000

    Added docblock and removed debug code.

--- src/ScraperBot/Storage/SqlLite3Storage.php
+++ src/ScraperBot/Storage/SqlLite3Storage.php
@@ -44,10 +44,13 @@ class SqlLite3Storage implements StorageInterface {
 
     }
 
-
+    /**
+     * Remove crawl identified by $id timestamp
+     *
+     * @param $id
+     *   Timestamp
+     */
     public function DeleteCrawl($id) {
-        echo 'removing crawl';
-
         $queryString = sprintf("DELETE FROM sites WHERE timestamp='%s';", $id);
         $this->pdo->query( $queryString);
         $queryString = sprintf("DELETE FROM tags WHERE timestamp='%s';", $id);
